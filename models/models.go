36af9151c4c5f1856542a1b96f552c59813c403b
mnurhad <nurhadi@nominavipindonesia.com>
2022-08-23 16:54:05 +0700

    Add Stuktur model

--- models/models.go
+++ models/models.go
@@ -23,3 +23,19 @@ type User struct {
 	Address_Details []Address          `json:"address" bson:"address"`
 	Order_Status    []Order            `json:"orders" bson:"orders"`
 }
+
+type Product struct {
+	Product_ID   primitive.ObjectID `bson:"_id"`
+	Product_Name *string            `json:"product_name"`
+	Price        *uint64            `json:"price"`
+	Rating       *uint8             `json:"rating"`
+	Image        *string            `json:"image"`
+}
+
+type ProductUser struct {
+	Product_ID   primitive.ObjectID `bson:"_id"`
+	Product_Name *string            `json:"product_name" bson:"product_name"`
+	Price        int                `json:"price"  bson:"price"`
+	Rating       *uint              `json:"rating" bson:"rating"`
+	Image        *string            `json:"image"  bson:"image"`
+}
