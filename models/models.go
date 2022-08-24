973777fcaabbe10d022b2a8f541dc496a235cbb0
mnurhad <nurhadi@nominavipindonesia.com>
2022-08-24 10:04:23 +0700

    Update Type struct Model

--- models/models.go
+++ models/models.go
@@ -39,3 +39,25 @@ type ProductUser struct {
 	Rating       *uint              `json:"rating" bson:"rating"`
 	Image        *string            `json:"image"  bson:"image"`
 }
+
+type Address struct {
+	Address_id primitive.ObjectID `bson:"_id"`
+	House      *string            `json:"house_name" bson:"house_name"`
+	Street     *string            `json:"street_name" bson:"street_name"`
+	City       *string            `json:"city_name" bson:"city_name"`
+	Pincode    *string            `json:"pin_code" bson:"pin_code"`
+}
+
+type Order struct {
+	Order_ID       primitive.ObjectID `bson:"_id"`
+	Order_Cart     []ProductUser      `json:"order_list"  bson:"order_list"`
+	Orderered_At   time.Time          `json:"ordered_on"  bson:"ordered_on"`
+	Price          int                `json:"total_price" bson:"total_price"`
+	Discount       *int               `json:"discount"    bson:"discount"`
+	Payment_Method Payment            `json:"payment_method" bson:"payment_method"`
+}
+
+type Payment struct {
+	Digital bool `json:"digital" bson:"digital"`
+	COD     bool `json:"cod"     bson:"cod"`
+}
